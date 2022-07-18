dec3c1d434741b9085b33d98f38511fb82b0b63e
22 <tequilak@protonmail.com>
2022-07-19 00:24:22 +0800

    1

--- tamper/safedog.py
+++ tamper/safedog.py
@@ -4,21 +4,21 @@
 Copyright (c) 2006-2022 sqlmap developers (http://sqlmap.org/)
 See the file 'LICENSE' for copying permission
 """
-import random
 import os
+import random
 from lib.core.enums import PRIORITY
 from lib.core.settings import UNICODE_ENCODING
-__priority__ = PRIORITY.LOW
 from lib.core.common import singleTimeWarnMessage
 from lib.core.data import kb
 from lib.core.enums import DBMS
+__priority__ = PRIORITY.LOW
 
 def dependencies():
     pass
 
 
 def dependencies():
-    singleTimeWarnMessage("Bypass safedog by l1b3ri'%s' only %s" % (os.path.basename(__file__).split(".")[0], DBMS.MYSQL))
+    singleTimeWarnMessage("汪汪队出发!'%s' only %s" % (os.path.basename(__file__).split(".")[0], DBMS.MYSQL))
 
 
 def tamper(payload, **kwargs):

